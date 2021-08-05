4ab11bbb6b13dfd8776531ca95750ed0d753cc76
Pamela Hammond <hammondpamelac@gmail.com>
2021-08-04 21:40:23 -0400

    init npm and installed inquirer, and gitignore node modules and ds store

---
+++ index.js
@@ -0,0 +1,21 @@
+// <!-- * Create a `.gitignore` file and include `node_modules/` and `.DS_Store/` so that your `node_modules` directory isn't tracked or uploaded to GitHub. Be sure to create your `.gitignore` file before installing any npm dependencies.
+
+// * Make sure that your repo includes a `package.json` with the required dependencies. You can create one by running `npm init` when you first set up the project, before installing any dependencies.
+
+// * Include any other screenshots you deem necessary to help someone who has never been introduced to your application understand the purpose and function of it. This is how you will communicate to potential employers or other developers in the future what you built and why, and to show how it works. -->
+
+// <!-- Must submit
+
+// * A sample README generated using the application must be submitted.
+
+// * Your GitHub repository containing your application code.
+
+// * A walkthrough video demonstrating the functionality of the application.
+
+// * A walkthrough video that demonstrates the functionality of the README generator must be submitted, and a link to the video should be included in your README file.
+
+// * The walkthrough video must demonstrate how a user would invoke the application from the command line.
+
+// * The walkthrough video must demonstrate how a user would enter responses to all of the prompts in the application.
+
+// * The walkthrough video must demonstrate a generated README that matches the user input and has a functioning table of contents. -->
