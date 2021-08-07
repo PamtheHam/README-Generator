7233e70f0816322715dd74627fc975f2bc00aaa7
Pamela Hammond <hammondpamelac@gmail.com>
2021-08-07 16:00:49 -0400

    added questions and text that will go into the README.md

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
-## README-Generator
+# README-Generator
+
+## Description
+
 In this project, I worked to develop a process that makes creating a README.md more quick and simple for the user. In order to demonstrate the application, I created a walk-through video on Screencastify where I show the user how to utilize the application to produce a README.md with the user's input using the inquirer package from NPM.
 
+## Table of Contents
+
+- [Description](#Description)
+- [User-Story](#User-Story)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Credits](#Credits)
+- [License](#License)
 
 
-User Story:
-AS A developer
-I WANT a README generator
-SO THAT I can quickly create a professional README for a new project
+
+## Installation
+
+
+## Usage
+
+## Credits
+
+## License
+
 
 
 
 [The walk-through video] () 
 [The repository] (https://github.com/PamtheHam/README-Generator)
+
 
 
--- index.js
+++ index.js
@@ -1,21 +1,97 @@
-// <!-- * Create a `.gitignore` file and include `node_modules/` and `.DS_Store/` so that your `node_modules` directory isn't tracked or uploaded to GitHub. Be sure to create your `.gitignore` file before installing any npm dependencies.
-
-// * Make sure that your repo includes a `package.json` with the required dependencies. You can create one by running `npm init` when you first set up the project, before installing any dependencies.
-
-// * Include any other screenshots you deem necessary to help someone who has never been introduced to your application understand the purpose and function of it. This is how you will communicate to potential employers or other developers in the future what you built and why, and to show how it works. -->
-
 // <!-- Must submit
 
 // * A sample README generated using the application must be submitted.
-
 // * Your GitHub repository containing your application code.
+// a link to the video should be included in your README file.
+// Include any other screenshots you deem necessary to help someone
+// the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-// * A walkthrough video demonstrating the functionality of the application.
 
-// * A walkthrough video that demonstrates the functionality of the README generator must be submitted, and a link to the video should be included in your README file.
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// * The walkthrough video must demonstrate how a user would invoke the application from the command line.
+const generateQuestions = () => {
+    inquirer.prompt([
+        {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your project.',
+          },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter a step-by-step instruction on how to install your project.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions or examples for the use of your project.',
+      },
+      {
+        type: 'input',
+        name: 'credits',
+        message: 'Please provide contact information as well as GitHub profiles on any collaborators for this project.',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Please detail any licenses that you have for this project and their description.',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address.',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username.',
+      },
+    ]);
+  };
 
-// * The walkthrough video must demonstrate how a user would enter responses to all of the prompts in the application.
+const generateReadMe = (answers) => {
 
-// * The walkthrough video must demonstrate a generated README that matches the user input and has a functioning table of contents. -->
+`${answers.title}
+
+## Description
+${answers.description}
+
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Credits
+${answers.credits}
+
+## License
+${answers.license}`
+
+}
+
+  
+  const init = () => {
+    generateQuestions()
+      .then((answers) => fs.writeFile('README.md', generateReadMe(answers)))
+      .then(() => console.log('Successfully wrote to README.md!'))
+      .catch((err) => console.error(err));
+  };
+  
+  init();
